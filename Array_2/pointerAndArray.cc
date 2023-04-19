98ce6ac9751b97872ef90c826c252905befd4fab
AnushkaBanerjee <anushkabanerjee03@gmail.com>
2023-04-19 23:46:37 +0530

    vectors

--- Array_2/pointerAndArray.cc
+++ Array_2/pointerAndArray.cc
@@ -2,11 +2,11 @@
 using namespace std;
 int main(){
     int arr[] = {4,2,41,1,7};
-    int* ptr = arr;//giving address /*TODO: int *ptr = &arr[0]*/
+    int* ptr = arr;//giving address /*TODO: int *ptr = &arr[0] */
     // cout<<ptr<<endl;
     // cout<<ptr[0]<<endl;
     for(int i = 0;i < 5;i++){
-        cout<<ptr[i]<<' ';
+        cout<<ptr[i]<<' ';//OR cout<<i[arr]<<endl; OR cout<<i[ptr]<<endl;
     }
     cout<<endl;
     *ptr = 8;
@@ -15,6 +15,8 @@ int main(){
         cout<<*ptr<<' ';
         ptr++;
     }
-
+    cout<<endl;
+    ptr = arr;
+    cout<<ptr[0]<<endl;
 return 0;
 }
