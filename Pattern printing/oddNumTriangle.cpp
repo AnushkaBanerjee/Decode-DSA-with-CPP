d4c7e1f1780841752b81466e0959986721136821
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-05-11 09:27:55 +0530

    recognised the mistake

--- Pattern printing/oddNumTriangle.cpp
+++ Pattern printing/oddNumTriangle.cpp
@@ -6,11 +6,12 @@ cout<<"No of rows:";
 cin>>n;
 
 for(int i = 1; i <= n;i++){
-    int a = 1;
-    for(int j = 1;j <= i;j++){//for(int j = 1;j <= 2*n-1;j+=2)
+    // int a = 1;
+    for(int j = 1;j <= 2*i-1;j+=2){//for(int j = 1;j <= i;j++)
         //first 'i' odd numbers
-        cout<<a;
-        a+=2;
+        // cout<<a;
+        // a+=2;
+        cout<<j<<" ";
     }
     cout<<endl;
 }
