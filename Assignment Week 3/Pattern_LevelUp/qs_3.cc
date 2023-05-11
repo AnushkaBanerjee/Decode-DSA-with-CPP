f23d47501e25b336b1461f510513ec5c80647322
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-05-11 22:59:49 +0530

    pyramid qs

---
+++ Assignment Week 3/Pattern_LevelUp/qs_3.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+ cout<<"Enter no of rows:";
+ cin>>n;
+ for(int i = 1;i <= n;i++){
+    for(int j = 1;j <= n-i;j++){
+        cout<<" ";
+    }
+    for(int k = i;k >= 1;k--){
+        cout<<(char)(k+64) ;
+    }
+    if(i>=2){
+        char a= 'B';
+        for(int j = 1;j < i;j++){
+            cout<<a;
+            a++;
+        }
+    }
+    cout<<endl;
+ }
+return 0;
+}
