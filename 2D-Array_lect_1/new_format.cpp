d68e41666f71fc29dded50f031b35c291abf2c8b
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-05-16 00:03:53 +0530

    new things

---
+++ 2D-Array_lect_1/new_format.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+string colours[] = {"Red", "Green", "Blue"};
+// To print the elements in the console:
+for(string colour : colours)
+cout << colour << ",";
+return 0;
+}
---
+++ FUNCTION_CPP/passByReference.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+// void swap(int &a,int &b){
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     cout<<&a<<&b<<" Address of func a and b -> 2\n";
+
+// }
+
+int main(){
+//  int a = 6,b = 8;
+//  cout<<" Before swap:"<<a<<" "<<b<<endl;
+//  cout<<&a<<&b<<" Address of main a and b\n";
+//  swap(a,b);
+//  cout<<" After swap:"<<a<<" "<<b<<endl;
+//  cout<<&a<<&b<<" Address of func a and b -> 1\n";
+// int d = 5;
+// int &c = d;
+// cout<<&c<<" "<<&d<<endl;
+// cout<<c;
+float x = 5.5;
+cout<<sizeof(x)<<endl;
+int a = 5;
+cout<<sizeof(a);
+return 0;
+}
