19fb2799c587e65682267150313ea858320b845f
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-06-25 12:28:25 +0530

    built_in_functions

---
+++ Strings Part 1/built_in_Functions.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+//TODO: <string_name>.size()
+// string str = "My Mother is the best!!";
+// cout<<str.size();//str.length()->{synonyms}
+
+// TODO: <string_name>.push_back('<character>')
+// string str = "abcd";
+// cout<<str<<endl;
+// str.push_back('e');
+// str.push_back('f');
+// str.push_back('g');
+// cout<<str<<endl;
+// str.push_back(char) -> we cannot push_back a string
+
+//TODO: <string_name>.pop_back()
+string str = "anushka";
+cout<<str<<endl;
+str.pop_back();
+cout<<str;
+return 0;
+}
