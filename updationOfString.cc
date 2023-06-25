ae606daac761cf8d2b4e5df63dfc01a58346b0f5
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-06-25 11:30:32 +0530

    updating  value in a string

---
+++ updationOfString.cc
@@ -0,0 +1,20 @@
+//Input a string of size n and update all the even positions in the string to character 'a'.Consider 0-based indexing
+#include<iostream>
+using namespace std;
+int main(){
+//FIXME: This format is not valid
+// char s;
+
+// for(char i = 0;s[i]!='\0';i++){
+//     cin>>s[i];
+// }
+string s;
+getline(cin,s);
+int count = 0;
+for(int i = 0 ;s[i]!='\0';i++){
+    count++;
+    if(count % 2 == 0) s[i]='a';
+}
+cout<<s;
+return 0;
+}
