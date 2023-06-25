0d94d9f51788bc467175dea4dbf4c7b5460eb3c7
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-06-25 12:34:31 +0530

    append

--- Strings Part 1/built_in_Functions.cc
+++ Strings Part 1/built_in_Functions.cc
@@ -15,9 +15,19 @@ int main(){
 // str.push_back(char) -> we cannot push_back a string
 
 //TODO: <string_name>.pop_back()
-string str = "anushka";
-cout<<str<<endl;
-str.pop_back();
-cout<<str;
+// string str = "anushka";
+// cout<<str<<endl;
+// str.pop_back();
+// cout<<str;
+
+//'+' operator
+//Ususally used to append values to a string
+ string s = "abc";
+ cout<<s ;
+ string t = "efg";
+ s = s+t;//or we can also write as s  = abc + t -> abcefg
+ //or s = s + "xyz" -> abcxyz
+ //or s = s + "a" -> abca
+ cout<<s;
 return 0;
 }
