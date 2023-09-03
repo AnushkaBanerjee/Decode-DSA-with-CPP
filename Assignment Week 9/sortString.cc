9e1d5a14937b5714fe6b0ead0e7be02edc982274
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-09-03 14:33:14 +0530

    sorting

--- Assignment Week 9/sortString.cc
+++ Assignment Week 9/sortString.cc
@@ -10,12 +10,16 @@ int main(){
     }
     cout<<endl;
     //bubble sort
+    
+    
     for(int i = 0;i < n-1;i++){
         bool flag = true;
         for(int j = 0;j < n-1-i;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                flag = false;
             }
+            if(flag == true) break;
         }
     }
 
