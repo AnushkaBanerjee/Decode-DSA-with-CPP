e8850c6b3147132e5ba180b464c1e37267bc110a
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-09-24 23:42:53 +0530

    Optimised O(log n) complexity

---
+++ Searching Part 1/missingPositiveElement.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[] = {0,1,2,3,5,6,7};
+int n = 6;
+
+//linear approach
+// for(int i = 0;i < n;i++){
+//     if( i != arr[i]) {
+//         cout<<i;
+//         break;
+// }
+// }
+//The above approach is in O(log n)
+
+//time complexity : O(n)
+
+int lo = 0;
+int hi = n-1;
+int ans = -1;
+while(lo<=hi){
+    int mid = lo + (hi-lo)/2;
+    if(arr[mid] == mid) lo = mid+1;
+    else{
+        ans = mid;
+        hi = mid-1;
+    }
+}
+
+cout<<ans;
+return 0;
+}
