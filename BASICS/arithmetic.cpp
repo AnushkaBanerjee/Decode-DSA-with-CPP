b2ceeb7a58f89d0279efb5b94419c958db3a47b0
Anushka Banerjee <anushkabanerjee03@gmail.com>
2023-12-26 18:53:45 +0530

    edited

--- BASICS/arithmetic.cpp
+++ BASICS/arithmetic.cpp
@@ -7,19 +7,18 @@ int main(){
     //increment and decrement operator
     int x = 5;
      cout<<x<<endl;
-    // //++x;//pre increment  -->x = x + 1
-    // cout<<x++<<endl;
-    // cout<<x<<endl;
-    // //x++;//post increment
-    // //cout<<x<<endl;
-    // //This will assign value first and then increment it
-    // x--;//x = x-1;
-    // cout<<x<<endl;
+    //  x = x + 1
+    // cout<<++x;//pre increment : This will increment value first and then assign it
+    // cout<<x++<<endl;//post increment : This will assign value first and then increment it
+    cout<<x<<endl;
+    
+    //Similarly the decrement operators work like : x = x-1;
+    // x-- or --x
+    
     
     //multiline comment can be given using ctrl+/
-    // cout<<--x<<endl;
-    // cout<<x--<<endl;
-    // cout<<x<<endl;
+    
+    
     
     
 
