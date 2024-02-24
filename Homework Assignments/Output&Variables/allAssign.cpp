4362db6d71dd133c45e6babf9fc0646a1f6dc23e
Anushka Banerjee <anushkabanerjee03@gmail.com>
2024-02-24 19:12:28 +0530

    restarting the course

---
+++ Homework Assignments/Output&Variables/allAssign.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+// QS_1 -> Print "Physics" and "Wallah" in different lines
+
+// cout<<"Physics"<<endl;
+// cout<<"Wallah";
+
+//Qs_2 -> Write a program to print 10 using 2 positive numbers less than 6 in C++ as output.
+// cout<<5+5<<endl;
+
+//Qs_3-> How much space does the following data types take?
+// int : 4 bytes
+// bool : 1 byte
+// float : 4 bytes
+
+//Qs_4-> What is the output of this program?
+// int main() {
+// int a = 4;
+// int b = 5;
+// a++, b--;//a=5,b = 4
+// cout << ++a << " " << b--;
+// }
+
+
+//Output : 6 4
+
+// Qs_5->Write a program to find the circumference of a circle with radius 10 in C++.
+// int r = 10;
+// float cirCum = 2*3.14*r;
+// cout<<cirCum;
+
+//Qs_6 -> How many of these can be a variable name ?
+// 01Pwskills - false
+// _FLOAT - true
+// int - false
+// FLOAT - true
+// You will succeed - false
+
+return 0;
+}
